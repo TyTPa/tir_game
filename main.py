b902fed6c70e29baeab615acbc410e272a4afad1
TyTPa <papushina.tatyana@gmail.com>
2025-02-09 13:34:30 +0300

    add const code

--- main.py
+++ main.py
@@ -8,25 +8,14 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
 
-icon = pygame.image.load("img/tir_1.webp")
-pygame.display.set_icon(icon)
-
-target_img = pygame.image.load("img/target.jpeg")
-target_width = 210
-target_height = 240
-
-target_x = random.randint(0, SCREEN_WIDTH - target_width)
-target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-
-color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 running = True
 
 icon = pygame.image.load("img/tir_1.webp")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.jpeg")
-target_width = 210
-target_height = 240
+target_img = pygame.image.load("img/target.jpg")
+target_width = 80
+target_height = 80
 
 target_x = random.randint(0, SCREEN_WIDTH - target_width)
 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
@@ -35,19 +24,6 @@ color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
 
 while running:
-    screen.fill(color)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_x, mouse_y = pygame.mouse.get_pos()
-            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-                target_x = random.randint(0, SCREEN_WIDTH - target_width)
-                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
-                print("цель поражена")
-            else:
-                print("не попал")
-    screen.blit(target_img, (target_x, target_y))
-    pygame.display.update()
+        pass
 
 pygame.quit()
