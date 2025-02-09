f5b09e973d5d516508566a28579778de0e40e826
TyTPa <papushina.tatyana@gmail.com>
2025-02-09 13:34:30 +0300

    add const code

--- main.py
+++ main.py
@@ -8,12 +8,23 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
 
+icon = pygame.image.load("img/tir_1.webp")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("img/target.jpeg")
+target_width = 210
+target_height = 240
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+
+color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 running = True
 
 icon = pygame.image.load("img/tir_1.webp")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target.jpeg")
+target_img = pygame.image.load("img/target.jpg")
 target_width = 80
 target_height = 80
 
@@ -22,7 +33,7 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-running = True
+
 while running:
     screen.fill(color)
     for event in pygame.event.get():
