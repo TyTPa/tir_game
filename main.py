30abfe0989a8fb6f282332b7570a31127eed98e4
TyTPa <papushina.tatyana@gmail.com>
2025-02-09 14:41:03 +0300

    проверка попадания

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ running = True
 icon = pygame.image.load("img/tir_1.webp")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("img/target3.jpeg")
+target_img = pygame.image.load("img/target.jpeg")
 target_width = 80
 target_height = 80
 
@@ -22,19 +22,21 @@ target_y = random.randint(0, SCREEN_HEIGHT - target_height)
 
 color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
 
-
+running = True
 while running:
-  screen.fill(color)
-  for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-                  running = False
-  if event.type == pygame.MOUSEBUTTONDOWN:
-      mouse_x, mouse_y = pygame.mouse.get_pos()
-      if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
-          target_x = random.randint(0, SCREEN_WIDTH - target_width)
-          target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.fill(color)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x < mouse_x < target_x + target_width and target_y < mouse_y < target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+                print("цель поражена")
+            else:
+                print("не попал")
+    screen.blit(target_img, (target_x, target_y))
+    pygame.display.update()
 
-  screen.blit(target_img, (target_x, target_y))
-
-pygame.display.update()
 pygame.quit()
